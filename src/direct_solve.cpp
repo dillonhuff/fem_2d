cdfb5fa71a032132c7aaf34e7e7c41903754a534
Dillon Huff <dillonhuff@gmail.com>
2017-04-08 21:12:07 -0700

    now trying to build up constraint numberings

--- src/direct_solve.cpp
+++ src/direct_solve.cpp
@@ -6,6 +6,8 @@
 
 namespace ublas = boost::numeric::ublas;
 
+using namespace std;
+
 namespace fem_2d {
 
   boost::numeric::ublas::vector<double>
@@ -19,13 +21,64 @@ namespace fem_2d {
 
     return v;
   }
-  
+
+  std::vector<vec2>
+  from_vector(const ublas::vector<double>& pts) {
+    vector<vec2> v;
+
+    for (unsigned i = 0; i < pts.size(); i += 2) {
+      v.push_back( vec2(pts(i), pts(i + 1)) );
+    }
+
+    return v;
+  }
+
+  ublas::matrix<double>
+  build_k_matrix(const trimesh& mesh) {
+    //ublas::vector<double> pts = to_vector(mesh.pts);
+    unsigned num_tris = mesh.tris.size();
+    ublas::matrix<double> k_inv = ublas::identity_matrix<float>(num_tris);
+
+    return k_inv;
+  }
+
+  void cull_by_constraints(const ublas::vector<double>& v,
+			   const std::vector<unsigned> to_delete) {
+  }
+
+  void cull_by_constraints(const ublas::matrix<double>& v,
+			   const std::vector<unsigned> to_delete) {
+  }
+
+  std::vector<unsigned>
+  build_num_constraints(const std::vector<constraint2>& constraints) {
+    vector<unsigned> cnums;
+    
+    return cnums;
+  }
+
   std::vector<vec2>
   compute_displacements(const trimesh& mesh,
 			const std::vector<constraint2>& constraints,
 			const std::vector<vec2>& forces) {
-    ublas::vector<double> fs = to_vector(forces);
-    return {};
+    ublas::vector<double> f = to_vector(forces);
+    ublas::matrix<double> k = build_k_matrix(mesh);
+
+    vector<unsigned> num_constraints = build_num_constraints(constraints);
+    cull_by_constraints(f, num_constraints);
+    cull_by_constraints(k, num_constraints);
+
+    ublas::matrix<double> k_inv = ublas::identity_matrix<float>(k.size1());
+    ublas::permutation_matrix<size_t> pm(k.size1());
+    lu_factorize(k, pm);
+    lu_substitute(k, pm, k_inv);    
+
+    ublas::vector<double> u = prod(k_inv, f);
+
+    cout << "u = " << u << endl;
+
+    return from_vector(u);
+
   }
 
 }

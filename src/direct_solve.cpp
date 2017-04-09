d73c13f0c54d7e4222164e709795229e1e16b040
Dillon Huff <dillonhuff@gmail.com>
2017-04-08 20:58:29 -0700

    Starting to make a real direct solver

--- src/direct_solve.cpp
+++ src/direct_solve.cpp
@@ -1,11 +1,30 @@
 #include "direct_solve.h"
 
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/lu.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+namespace ublas = boost::numeric::ublas;
+
 namespace fem_2d {
 
+  boost::numeric::ublas::vector<double>
+  to_vector(const std::vector<vec2> pts) {
+    ublas::vector<double> v(2*pts.size());
+
+    for (unsigned i = 0; i < pts.size(); i++) {
+      v(2*i) = pts[i].x();
+      v(2*i + 1) = pts[i].y();
+    }
+
+    return v;
+  }
+  
   std::vector<vec2>
   compute_displacements(const trimesh& mesh,
 			const std::vector<constraint2>& constraints,
 			const std::vector<vec2>& forces) {
+    ublas::vector<double> fs = to_vector(forces);
     return {};
   }
 

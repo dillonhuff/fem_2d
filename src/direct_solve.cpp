1b2f49cc5195073973611ae85e966c1ff784b146
Dillon Huff <dillonhuff@gmail.com>
2017-04-08 22:28:21 -0700

    Now trying to get a real K matrix constructed

--- src/direct_solve.cpp
+++ src/direct_solve.cpp
@@ -101,8 +101,52 @@ namespace fem_2d {
     cout << "D = " << endl;
     cout << D << endl;
 
-    ublas::matrix<double> B = ublas::zero_matrix<double>(3, 6);
+    vertex_triangle t = mesh.tris[elem_ind];
 
+    double y1 = mesh.verts[t.verts[0]].y();
+    double y2 = mesh.verts[t.verts[1]].y();
+    double y3 = mesh.verts[t.verts[2]].y();
+
+    double y23 = y2 - y3;
+    double y31 = y3 - y1;
+    double y12 = y1 - y2;
+    double y13 = y1 - y3;
+
+    double x1 = mesh.verts[t.verts[0]].x();
+    double x2 = mesh.verts[t.verts[1]].x();
+    double x3 = mesh.verts[t.verts[2]].x();
+
+    double x32 = x3 - x2;
+    double x13 = x1 - x3;
+    double x21 = x2 - x1;
+    double x23 = x2 - x3;
+
+    ublas::matrix<double> B = ublas::zero_matrix<double>(3, 6);
+    B(0, 0) = y23;
+    B(0, 1) = 0;
+    B(0, 2) = y31;
+    B(0, 3) = 0;
+    B(0, 4) = y12;
+    B(0, 5) = 0;
+
+    B(1, 0) = 0;
+    B(1, 1) = x32;
+    B(1, 2) = 0;
+    B(1, 3) = x13;
+    B(1, 4) = 0;
+    B(1, 5) = x21;
+
+    B(2, 0) = 0;
+    B(2, 1) = 0;
+    B(2, 2) = 0;
+    B(2, 3) = 0;
+    B(2, 4) = 0;
+    B(2, 5) = 0;
+
+    double detJ = x13*y23 - y13*x23;
+
+    B = (1.0 / detJ) * B;
+    
     D = thickness*area*D;
     ublas::matrix<double> B_t = trans(B);
     ublas::matrix<double> DB = prod(D, B);

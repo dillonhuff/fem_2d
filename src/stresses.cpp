922106b28db945c7512d98da5bd54caa8a397009
Dillon Huff <dillonhuff@gmail.com>
2017-06-26 22:10:19 -0700

    Need global D matrix constructino

--- src/stresses.cpp
+++ src/stresses.cpp
@@ -1,9 +1,23 @@
 #include "stresses.h"
 
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/lu.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+namespace ublas = boost::numeric::ublas;
+
 namespace fem_2d {
 
+  ublas::matrix<double> 
+  build_d_matrix(const std::vector<vec2>& displacements) {
+    return {};
+  }
+  
   std::vector<double>
   compute_stresses(const std::vector<vec2>& displacements) {
+    ublas::matrix<double> d_matrix =
+      build_d_matrix(displacements);
+
     return {};
   }
   

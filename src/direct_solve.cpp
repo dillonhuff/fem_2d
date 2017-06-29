d4f26b24fcb9bd1793491c81ed377f900bcc5c5e
Dillon Huff <dillonhuff@gmail.com>
2017-06-29 01:05:03 -0700

    Working on isolating B and D matrices

--- src/direct_solve.cpp
+++ src/direct_solve.cpp
@@ -68,6 +68,52 @@ namespace fem_2d {
   }
 
   ublas::matrix<double>
+  build_element_B_matrix(const unsigned elem_ind,
+			 const trimesh& mesh) {
+    vertex_triangle t = mesh.tris[elem_ind];
+
+    double y1 = mesh.verts[t.verts[0]].y();
+    double y2 = mesh.verts[t.verts[1]].y();
+    double y3 = mesh.verts[t.verts[2]].y();
+
+    double y23 = y2 - y3;
+    double y31 = y3 - y1;
+    double y12 = y1 - y2;
+
+    double x1 = mesh.verts[t.verts[0]].x();
+    double x2 = mesh.verts[t.verts[1]].x();
+    double x3 = mesh.verts[t.verts[2]].x();
+
+    double x32 = x3 - x2;
+    double x13 = x1 - x3;
+    double x21 = x2 - x1;
+
+    ublas::matrix<double> B = ublas::zero_matrix<double>(3, 6);
+    B(0, 0) = y23;
+    B(0, 1) = 0;
+    B(0, 2) = y31;
+    B(0, 3) = 0;
+    B(0, 4) = y12;
+    B(0, 5) = 0;
+
+    B(1, 0) = 0;
+    B(1, 1) = x32;
+    B(1, 2) = 0;
+    B(1, 3) = x13;
+    B(1, 4) = 0;
+    B(1, 5) = x21;
+
+    B(2, 0) = x32;
+    B(2, 1) = y23;
+    B(2, 2) = x13;
+    B(2, 3) = y31;
+    B(2, 4) = x21;
+    B(2, 5) = y12;
+
+    return B;
+  }
+
+  ublas::matrix<double>
   build_element_k_matrix(const unsigned elem_ind, const trimesh& mesh) {
     unsigned dof = 2*mesh.verts.size();
 

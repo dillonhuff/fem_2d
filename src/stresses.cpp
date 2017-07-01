8a620e3193f5228ff98c55bf446a783b688bcfe4
Dillon Huff <dillonhuff@gmail.com>
2017-06-30 22:06:45 -0700

    Stresses dont match up with what was originally computed

--- src/stresses.cpp
+++ src/stresses.cpp
@@ -6,29 +6,23 @@ using namespace std;
 
 namespace fem_2d {
 
-  ublas::matrix<double> 
-  build_d_matrix(const std::vector<vec2>& displacements) {
-    return {};
-  }
-  
   std::vector<double>
   compute_stresses(const trimesh& mesh,
 		   const std::vector<constraint2>& constraints,
 		   const std::vector<vec2>& displacements) {
-    ublas::matrix<double> d_matrix =
-      build_d_matrix(displacements);
 
+    cout << "# of displacements = " << displacements.size() << endl;
     auto D = build_D_matrix();
     for (int i = 0; i < mesh.tris.size(); i++) {
       ublas::vector<double> v(6);
 
       vertex_triangle t = mesh.tris[i];
-      v(0) = mesh.verts[t.verts[0]].x();
-      v(1) = mesh.verts[t.verts[0]].y();
-      v(2) = mesh.verts[t.verts[1]].x();
-      v(3) = mesh.verts[t.verts[1]].y();
-      v(4) = mesh.verts[t.verts[2]].x();
-      v(5) = mesh.verts[t.verts[2]].y();
+      v(0) = displacements[t.verts[0]].x();
+      v(1) = displacements[t.verts[0]].y();
+      v(2) = displacements[t.verts[1]].x();
+      v(3) = displacements[t.verts[1]].y();
+      v(4) = displacements[t.verts[2]].x();
+      v(5) = displacements[t.verts[2]].y();
 
       // cout << 2*t.verts[0] << endl;
       // cout << 2*t.verts[0] + 1 << endl;

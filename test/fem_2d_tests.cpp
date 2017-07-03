86391efb4343a789a239dbf9405bdb795a2563df
Dillon Huff <dillonhuff@gmail.com>
2017-07-02 22:57:00 -0700

    Now trying to get an exact match of example data to actual test case

--- test/fem_2d_tests.cpp
+++ test/fem_2d_tests.cpp
@@ -16,10 +16,10 @@ namespace fem_2d {
 
   TEST_CASE("2 triangle system") {
     vector<vec2> points;
-    points.push_back(vec2(0, 0));
     points.push_back(vec2(3, 0));
     points.push_back(vec2(3, 2));
     points.push_back(vec2(0, 2));
+    points.push_back(vec2(0, 0));
 
     vector<vertex_triangle> tris;
     tris.push_back({0, 1, 2});
@@ -29,14 +29,14 @@ namespace fem_2d {
 
     vector<vec2> forces;
     forces.push_back(vec2(0, 0));
-    forces.push_back(vec2(0, 0));
     forces.push_back(vec2(0, -1000));
+    forces.push_back(vec2(0, 0));
     forces.push_back(vec2(0, 0));
 
     vector<constraint2> constraints;
-    constraints.push_back({0, XY_FIXED});
+    constraints.push_back({2, XY_FIXED});
     constraints.push_back({3, XY_FIXED});
-    constraints.push_back({1, Y_FIXED});
+    constraints.push_back({0, Y_FIXED});
 
     vector<vec2> displacements =
       compute_displacements(mesh, constraints, forces);
